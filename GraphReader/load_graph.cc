8df8fc2e47e364a16fd7dd66d211f77876902164
Mahyar Samani <msamani@ucdavis.edu>
2022-09-06 16:38:07 -0700

    Adding help promopt to loader binary.

--- GraphReader/load_graph.cc
+++ GraphReader/load_graph.cc
@@ -4,24 +4,27 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    if (argc != 6) {
-        throw -1;
+    if (argc == 1) {
+        std::cout << "Help prompt for this binary:" << std::endl;
+        std::cout << "Please provide these input arguments." << std::endl
+        << "[graph_path: str] [weighted: bool] [num_gpts: int] "
+        << "[atom_size: int] [out_path: str]." << std::endl;
+        return 0;
+    } else if (argc != 6) {
+        std::cout << "Wrong number of input arguments." << std::endl;
+        std::cout << "Please provide these input arguments." << std::endl
+        << "[graph_path: str] [weighted: bool] [num_gpts: int] "
+        << "[atom_size: int] [out_path: str]." << std::endl;
+        return 0;
     }
 
-    std::string graph_file_name = argv[1];
-    std::string mode = argv[2];
-    int atom_size = std::stoi(argv[3]);
-    std::string outdir = argv[4];
-    int num_mpus = std::stoi(argv[5]);
+    std::string graph_path = argv[2];
+    bool weighted = argv[3] == "true";
+    int num_nodes = std::stoi(argv[4]);
+    int atom_size = std::stoi(argv[5]);
+    std::string out_path = argv[6];
 
-    bool is_weighted;
-    if (mode == "weighted") {
-        is_weighted = true;
-    } else {
-        is_weighted = false;
-    }
-
-    GraphReader graph_reader(graph_file_name, is_weighted, atom_size, outdir, num_mpus);
+    GraphReader graph_reader(graph_path, weighted, atom_size, out_path, num_nodes);
 
     graph_reader.createBinaryFiles();
 

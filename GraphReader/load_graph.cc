b341553de31558e1a71e30596d1da5551d7c2a11
Mahyar Samani <msamani@ucdavis.edu>
2022-09-07 11:47:51 -0700

    Adding GraphGen and removing graphs.

--- GraphReader/load_graph.cc
+++ GraphReader/load_graph.cc
@@ -8,21 +8,21 @@ int main(int argc, char** argv) {
         std::cout << "Help prompt for this binary:" << std::endl;
         std::cout << "Please provide these input arguments." << std::endl
         << "[graph_path: str] [weighted: bool] [num_gpts: int] "
-        << "[atom_size: int] [out_path: str]." << std::endl;
+        << "[atom_size: int] [out_path: str]" << std::endl;
         return 0;
     } else if (argc != 6) {
-        std::cout << "Wrong number of input arguments." << std::endl;
+        std::cout << "Wrong number of input arguments: " << std::endl;
         std::cout << "Please provide these input arguments." << std::endl
         << "[graph_path: str] [weighted: bool] [num_gpts: int] "
-        << "[atom_size: int] [out_path: str]." << std::endl;
+        << "[atom_size: int] [out_path: str]" << std::endl;
         return 0;
     }
 
-    std::string graph_path = argv[2];
-    bool weighted = argv[3] == "true";
-    int num_nodes = std::stoi(argv[4]);
-    int atom_size = std::stoi(argv[5]);
-    std::string out_path = argv[6];
+    std::string graph_path = argv[1];
+    bool weighted = argv[2] == "true";
+    int num_nodes = std::stoi(argv[3]);
+    int atom_size = std::stoi(argv[4]);
+    std::string out_path = argv[5];
 
     GraphReader graph_reader(graph_path, weighted, atom_size, out_path, num_nodes);
 

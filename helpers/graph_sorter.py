3a87691a5fb8b6e050865d8ac8114d4dec467fb9
Mahyar Samani <msamani@ucdavis.edu>
2022-10-08 21:13:40 -0700

    Moving graph_sorter and reindexing vertices.

--- helpers/graph_sorter.py
+++ helpers/graph_sorter.py
@@ -15,16 +15,16 @@ if __name__ == "__main__":
         for line in input_graph_file.readlines():
             if line.startswith("#"):
                 continue
-            src_id, dst_id = line.split()
+            src_id, dst_id = line.split()[:2]
             src_id = int(src_id)
             dst_id = int(dst_id)
             edges.append((src_id, dst_id))
 
-    edges = sorted(edges, key = lambda x: (x[0], x[1]))
-
     edges =  list(dict.fromkeys(edges))
-    edges = [e for e in edges if e[0] != e[1]]
-    
+    edges = sorted(edges, key = lambda x: (x[0], x[1]))
+    min_id = edges[0][0]
+    edges = [(e[0]-min_id, e[1]-min_id) for e in edges if e[0] != e[1]]
+
     with open(args.output_graph_file, "w") as output_graph_file:
         for src_id, dst_id in edges:
             output_graph_file.write(f"{src_id} {dst_id}\n")

f7a77a66e3b7f4fd73113bcd75ae5d384b005a47
Mahyar Samani <msamani@ucdavis.edu>
2022-11-11 14:43:22 -0800

    Updating graph_sorter to balance load.

--- helpers/graph_sorter.py
+++ helpers/graph_sorter.py
@@ -1,6 +1,6 @@
 #!/bin/python
 import argparse
-from itertools import groupby
+from math import ceil
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -12,13 +12,47 @@ if __name__ == "__main__":
 
     edges = []
     with open(args.input_graph_file, "r") as input_graph_file:
+        degrees = {}
+        for line in input_graph_file.readlines():
+            if line.startswith("#"):
+                continue
+            src_id = line.split()[0]
+            if src_id in degrees:
+                degrees[src_id] += 1
+            else:
+                degrees[src_id] = 1
+        degrees = [(src, deg) for src, deg in degrees.items()]
+        degrees = sorted(degrees, reverse=True, key=lambda x: x[1])
+
+        even_id = 0
+        odd_id = 1
+        max_translation_id = 0
+        translate_table = {}
+        for i in range(ceil(len(degrees)/2)):
+            big_src = degrees[i][0]
+            little_src = degrees[len(degrees)-1-i][0]
+            translate_table[big_src] = even_id
+            max_translation_id = even_id
+            even_id += 2
+            if len(degrees)-1-i > i:
+                translate_table[little_src] = odd_id
+                max_translation_id = odd_id
+                odd_id += 2
+        first_avail_id = max_translation_id + 1
+
+        input_graph_file.seek(0)
         for line in input_graph_file.readlines():
             if line.startswith("#"):
                 continue
             src_id, dst_id = line.split()[:2]
-            src_id = int(src_id)
-            dst_id = int(dst_id)
-            edges.append((src_id, dst_id))
+            new_src_id = translate_table[src_id]
+            if dst_id in translate_table:
+                new_dst_id = translate_table[dst_id]
+            else:
+                new_dst_id = first_avail_id
+                translate_table[dst_id] = new_dst_id
+                first_avail_id += 1
+            edges.append((new_src_id, new_dst_id))
 
     edges =  list(dict.fromkeys(edges))
     edges = sorted(edges, key = lambda x: (x[0], x[1]))
